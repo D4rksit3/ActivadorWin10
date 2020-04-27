ac4b8a1412a51d3143609909b3d2001ca47108a8
dark186 <gonzaloroque21@gmail.com>
2020-04-27 11:34:04 -0500

    2commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+import py2exe
+
+setup(name="ActivateWin10",
+version="1.0",
+description="Activa tu win 10 de manera facil.",
+author="D4rkSit3"
+author_email="",
+url="https://facebook.com/d4rksit3",
+scripts="sudo.exe,activador.py,console=["activador.py,options={"py2exe": {"bundle_files": 2}},
+zipfile=none,
+
+
+)
