ff14ddadffbd2c5e132237626d9f18744b8f0ba6
dark186 <gonzaloroque21@gmail.com>
2020-04-27 11:23:48 -0500

    mi first commit

---
+++ activador.py
@@ -0,0 +1,334 @@
+import time
+import os
+
+os.system("cls")
+print("""\u001b[31m                 XXXXXXX X XXXXXXXXXXX XX X  X XXXXXXXXXXXXXXXXXXXXXX
+              \u001b[33mXXXXXXXXXXXXXXXXXXXXXXXX XXXX XXXXX XXXXXXXXXXXXXXXXXXX
+             XX XXX   XXXXXX                          X XX X    X XXXX
+             X XX  XXXX                                     XXXXXXXXXXXXXXXXX
+         XXXXXXX XXX                                                  XXXXXXXXXXXX
+         X XXX   X                                                           XXXXXXXX
+ XXXXXXXXXXX  XXXX              \u001b[34mDarkSite Microsoft W10                       XXXXXXXXXXXXX
+X XXXXXXXXXXXX                                                                           X
+XX                                                                            XXX        X
+X                                                                          XXX XXXXXXXXXXX
+XXXXXXXXXXXX     X                                                   XXX\u001b[36mXXXXXXXXXXXXXXXXX
+           XXXXXXXXX                                              XXXXXXXX XX XX
+                 X XX XXXXX X  XXX \u001b[37mXX X XXXXXXX  XX XXXXXXXXXXXXXXXXXXXXXXXXXXX
+                 XX  XXXXXXXXXXXXXXXXXXXX X X X X X  XXXXXXXXXX X  XXX
+                  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX XXXXX""")
+
+
+
+
+
+
+def salir():
+    os.system("cls")
+    time.sleep(0.2)
+    print("Saliendo del Script")
+    time.sleep (0.3)
+    os.system("cls")
+    time.sleep (0.5)
+    os.system("exit")
+
+def version():
+    os.system("cmd /c winver")
+
+
+def menu():
+    time.sleep(2)
+    os.system("cls")
+    
+    os.system("sudo.exe")
+     
+    print("Sucefull install sudo!")
+    time.sleep(0.5)
+    os.system("cls")
+    time.sleep(0.3)
+    os.system("sudo winver")
+    time.sleep(0.2)
+    print ("\u001b[33mActivador de MICOSOFT W10")
+
+
+
+
+
+
+    
+    print ("\n")
+    time.sleep(0.3)
+    print("Elige la version según tus caracteristicas: ")
+    print("Siguenos en Facebook https://facebook.com/d4rksit3")
+    print ("\n")
+    print ("\u001b[36m[1]-Windows 10 Home")
+    print ("[2]-Windows 10 Home N")
+    print ("[3]-Windows 10 Single Lenguaje")
+    print ("[4]-Windows 10 Country Specific")
+    print ("[5]-Windows 10 Professional")
+    print ("[6]-Windows 10 Professional N")
+    print ("[7]-Windows 10 Enterprise")
+    print ("[8]-Windows 10 Enterprise N")
+    print ("[9]-Windows 10 Education")
+    print ("[10]-Windows 10 Education N")
+    print ("[11]-Windows 10 Enterprise 2015 LTSB")
+    print ("[12]-Windows 10 Enterprise 2015 LTSB N")
+    print ("[13]-Autor")
+    print ("[00]-Exit")
+    print ("\n")
+    time.sleep(0.3)
+    
+    nom = int(input("\u001b[32mDarkSite-> "))
+
+    if nom == 0 or nom == 00:
+        salir()
+
+        
+    elif nom == 1:
+        os.system("cls")
+        time.sleep(0.3)
+        
+        #os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")
+        
+    
+    elif nom == 2:
+        os.system("cls")
+        time.sleep(0.3)
+        
+        #os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")
+        
+
+    elif nom == 3:
+        os.system("cls")
+        time.sleep(0.3)
+        
+        #os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")
+        
+
+
+
+    elif nom == 4:
+        os.system("cls")
+        time.sleep(0.3)
+        
+        #os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")
+        
+
+
+
+
+
+
+
+
+    elif nom == 5:
+        os.system("cls")
+        time.sleep(0.3)
+        
+
+       # os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")
+
+        
+        #slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+        #time.sleep(0.6)
+        
+
+    elif nom == 6:
+        os.system("cls")
+        time.sleep(0.3)
+        
+
+       # os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")
+
+
+    elif nom == 7:
+        os.system("cls")
+        time.sleep(0.3)
+        
+
+       # os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")
+    
+
+    elif nom == 8:
+        os.system("cls")
+        time.sleep(0.3)
+        
+
+       # os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")
+
+    elif nom == 9:
+        os.system("cls")
+        time.sleep(0.3)
+        
+
+       # os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")    
+
+
+    elif nom == 10:
+        os.system("cls")
+        time.sleep(0.3)
+        
+
+       # os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")
+
+    elif nom == 11:
+        os.system("cls")
+        time.sleep(0.3)
+        
+
+       # os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk WNMTR-4C88C-JK8YV-HQ7T2-76DF9")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")
+
+
+    elif nom == 12:
+        os.system("cls")
+        time.sleep(0.3)
+        
+
+       # os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+        time.sleep(0.3)
+        os.system("sudo cmd /c slmgr /ipk 2F77B-TNFGY-69QQF-B8YKP-D69TJ")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /skms kms.digiboy.ir")
+        time.sleep(5)
+        os.system("sudo cmd /c slmgr /ato")
+        
+        time.sleep(11)
+        print ("Sucefull install!")
+
+    elif nom == 13:
+        os.system("cls")
+        time.sleep(0.3)
+        
+        os.system("start https://facebook.com/d4rksit3")
+
+       # os.system("net user")
+        #data = input("Ingresa el user a darle permisos de administrador: ")
+       
+
+
+
+
+
+
+
+
+
+
+    
+
+
+#reset()
+
+if __name__ == "__main__":
+    menu()
